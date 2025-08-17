fb6cf165a09cc7c0025ef5dc339d6fabdf0165b0
Killian-bl <kilian.bastardy@gmail.com>
2025-08-17 19:56:03 +0200

    tentative relier back-front

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,5 +1,5 @@
 import ProductCard from "../components/ProductCard.tsx";
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import type { productType } from "../@types/ProductType";
 
 interface ProductsPageProps {
@@ -8,7 +8,7 @@ interface ProductsPageProps {
 
 const Product = ({ searchQuery }: ProductsPageProps) => {
 
-    const mockProducts:  productType[] = [
+    /*const mockProducts:  productType[] = [
         {
             id: 1,
             name: "Chaise design",
@@ -27,7 +27,29 @@ const Product = ({ searchQuery }: ProductsPageProps) => {
             price: 8,
             overview: "Lampe au style rétro pour éclairer votre pièce"
         }
-    ];
+    ];*/
+
+    const [products, setProducts] = useState<productType[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/product")
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error("Erreur réseau");
+                }
+                return res.json();
+            })
+            .then(data => {
+                setProducts(data);
+                setLoading(false);
+            })
+            .catch(err => {
+                setError(err.message);
+                setLoading(false);
+            });
+    }, []);
 
     const [quantities, setQuantities] = useState<Record<number, number>>({});
 
@@ -38,9 +60,12 @@ const Product = ({ searchQuery }: ProductsPageProps) => {
         }));
     };
 
-    const filteredProducts = mockProducts.filter((product) =>
+    const filteredProducts = products.filter((product) =>
         product.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
+
+    if (loading) return <p>Chargement...</p>;
+    if (error) return <p style={{ color: "red" }}>Erreur : {error}</p>;
 
 return (
     <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
